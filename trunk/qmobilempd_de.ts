a32a42e6043f4b0daee9bcf8aace5774cf65f43b
hendrikborghorst@googlemail.com <hendrikborghorst@googlemail.com@0b8954aa-1a55-b7e0-b578-22315a362590>
2011-07-22 12:44:23 +0000

    Changes to network stack. Hope this accelerate playlist fetchign

--- trunk/qmobilempd_de.ts
+++ trunk/qmobilempd_de.ts
@@ -2,6 +2,82 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="de_DE">
 <context>
+    <name>CurrentSongWidget</name>
+    <message>
+        <location filename="currentsongwidget.ui" line="20"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="96"/>
+        <source>Track-Nr.:</source>
+        <translation>Lied-Nr.:</translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="116"/>
+        <source>Bitrate:</source>
+        <translation>Bitrate:</translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="136"/>
+        <source>Path:</source>
+        <translation>Pfad:</translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="156"/>
+        <source>Title:</source>
+        <translation>Titel:</translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="56"/>
+        <source>Album:</source>
+        <translation>Album:</translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="76"/>
+        <source>Artist:</source>
+        <translation>Interpret:</translation>
+    </message>
+    <message>
+        <source>FileUri:</source>
+        <translation type="obsolete">Dateipfad:</translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="275"/>
+        <source>0:00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="295"/>
+        <source>99:99</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Title: </source>
+        <translation type="obsolete">Titel: </translation>
+    </message>
+    <message>
+        <source>Album: </source>
+        <translation type="obsolete">Album: </translation>
+    </message>
+    <message>
+        <source>Artist: </source>
+        <translation type="obsolete">Interpret: </translation>
+    </message>
+    <message>
+        <source>Bitrate: </source>
+        <translation type="obsolete">Bitrate: </translation>
+    </message>
+    <message>
+        <source>Path: </source>
+        <translation type="obsolete">Pfad: </translation>
+    </message>
+    <message>
+        <source>Track-Nr.: </source>
+        <translation type="obsolete">Lied-Nr.: </translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="20"/>
@@ -39,74 +115,79 @@
         <translation>Zeige Lautstärkenkontrolle</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="68"/>
+        <location filename="mainwindow.cpp" line="69"/>
         <source>Connect to host</source>
         <translation>Mit Server verbinden</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="70"/>
+        <location filename="mainwindow.cpp" line="71"/>
         <source>Settings</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="72"/>
+        <source>Show Song info</source>
+        <translation type="unfinished">Zeige Lied Informationen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="74"/>
         <source>Artists</source>
         <translation>Interpreten</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="73"/>
+        <location filename="mainwindow.cpp" line="75"/>
         <source>Albums</source>
         <translation>Alben</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="74"/>
+        <location filename="mainwindow.cpp" line="76"/>
         <source>Playlist</source>
         <translation>Wiedergabelisten</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="75"/>
+        <location filename="mainwindow.cpp" line="77"/>
         <source>Files</source>
         <translation>Dateien</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="76"/>
+        <location filename="mainwindow.cpp" line="78"/>
         <source>About</source>
         <translation>Programminfo</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="133"/>
+        <location filename="mainwindow.cpp" line="139"/>
         <source>Default Server</source>
         <translation>Standard-Server</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="133"/>
+        <location filename="mainwindow.cpp" line="139"/>
         <source>Please define a default server under server properties which is used for connection.</source>
         <translation>Bitte definieren sie einen standard Server,welcher zur Verbindung genutzt wird.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="273"/>
+        <location filename="mainwindow.cpp" line="285"/>
         <source>qmobilempd</source>
         <translation type="unfinished">qmobileMPD</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="273"/>
+        <location filename="mainwindow.cpp" line="285"/>
         <source>qMobileMPD Version:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="273"/>
+        <location filename="mainwindow.cpp" line="285"/>
         <source> Copyright 2011 by Hendrik Borghorst, licensed under GPLv3 for more information visit: &lt;a href=&apos;http://code.google.com/p/qmobilempd&apos;&gt;Homepage&lt;/a&gt; . This applications uses &lt;a href=&apos;http://www.tango-project.org&apos;&gt;Tango icons&lt;/a&gt;.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="419"/>
+        <location filename="mainwindow.cpp" line="421"/>
         <source>Connection error</source>
         <translation type="unfinished">Verbindungsfehler</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="419"/>
+        <location filename="mainwindow.cpp" line="421"/>
         <source>Could not connect to default host check settings</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Kann nicht zum Server verbinden, bitte standard-Einstellungen überprüfen</translation>
     </message>
     <message>
         <source>qmobilempd</source>
@@ -114,12 +195,12 @@
         <translation type="obsolete">qmobileMPD</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="408"/>
+        <location filename="mainwindow.cpp" line="410"/>
         <source>Not authenticated</source>
         <translation>Nicht authentifiziert</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="408"/>
+        <location filename="mainwindow.cpp" line="410"/>
         <source>Functions could be limited without password, check settings and reconnect</source>
         <translation>Funktionsumfang könnte limitiert sein ohne Passwort.</translation>
     </message>
@@ -197,27 +278,27 @@
     <message>
         <location filename="ui_contextview.ui" line="113"/>
         <source>add item</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">hinzufügen</translation>
     </message>
     <message>
         <location filename="ui_contextview.ui" line="136"/>
         <source>play this item</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">abspielen</translation>
     </message>
     <message>
         <location filename="ui_contextview.ui" line="159"/>
         <source>clear playlist</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Wiedergabeliste löschen</translation>
     </message>
     <message>
         <location filename="ui_contextview.ui" line="182"/>
         <source>open playlist</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Wiedergabeliste öffnen</translation>
     </message>
     <message>
         <location filename="ui_contextview.ui" line="205"/>
         <source>save playlist</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Wiedergabeliste abspeichern</translation>
     </message>
     <message>
         <location filename="ui_contextview.ui" line="228"/>
@@ -225,68 +306,68 @@
         <translation>Anwendung beenden</translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="167"/>
+        <location filename="ui_contextview.cpp" line="168"/>
         <source>Artists:</source>
         <translation>Interpreten:</translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="214"/>
+        <location filename="ui_contextview.cpp" line="215"/>
         <source>Albums:</source>
         <translation>Alben:</translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="341"/>
+        <location filename="ui_contextview.cpp" line="342"/>
         <source>Current playlist:</source>
         <translation>aktuelle Wiedergabeliste:</translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="658"/>
-        <location filename="ui_contextview.cpp" line="721"/>
+        <location filename="ui_contextview.cpp" line="664"/>
+        <location filename="ui_contextview.cpp" line="749"/>
         <source>Do you really want to add ALL albums to current playlist?</source>
         <translation>Möchten sie wirklich ALLE Alben zur aktuellen Wiedergabeliste hinzufügen?</translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="866"/>
+        <location filename="ui_contextview.cpp" line="976"/>
         <source>Name</source>
         <translation>Name</translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="866"/>
+        <location filename="ui_contextview.cpp" line="976"/>
         <source>Please enter name for playlist.</source>
         <translation>Bitte geben sie einen Namen für die Wiedergabeliste ein.</translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="874"/>
+        <location filename="ui_contextview.cpp" line="984"/>
         <source>Error</source>
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="874"/>
+        <location filename="ui_contextview.cpp" line="984"/>
         <source>Sorry save list could not get saved</source>
         <translation>Wiedergabeliste konnte nicht gespeichert werden.</translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="900"/>
+        <location filename="ui_contextview.cpp" line="1010"/>
         <source>Saved playlists:</source>
         <translation>gespeicherte Wiedergabelisten:</translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="907"/>
+        <location filename="ui_contextview.cpp" line="1017"/>
         <source>Clear playlist</source>
         <translation>Wiedergabeliste löschen</translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="907"/>
+        <location filename="ui_contextview.cpp" line="1017"/>
         <source>Are you sure to delete current playlist?</source>
         <translation>Möchten sie wirklich die aktuelle Wiedergabeliste löschen?</translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="919"/>
+        <location filename="ui_contextview.cpp" line="1029"/>
         <source>Quit?</source>
         <translation>Beenden?</translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="919"/>
+        <location filename="ui_contextview.cpp" line="1029"/>
         <source>Are you sure to quit?</source>
         <translation>Möchten sie wirklich das Programm beenden?</translation>
     </message>
@@ -311,7 +392,7 @@
     <message>
         <location filename="ui_songinfo.ui" line="86"/>
         <source>Album:</source>
-        <translation>Albuim:</translation>
+        <translation type="unfinished">Album:</translation>
     </message>
     <message>
         <location filename="ui_songinfo.ui" line="99"/>
--- trunk/qmobilempd_en.ts
+++ trunk/qmobilempd_en.ts
@@ -2,6 +2,54 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="en_US">
 <context>
+    <name>CurrentSongWidget</name>
+    <message>
+        <location filename="currentsongwidget.ui" line="20"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="56"/>
+        <source>Album:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="76"/>
+        <source>Artist:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="96"/>
+        <source>Track-Nr.:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="116"/>
+        <source>Bitrate:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="136"/>
+        <source>Path:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="156"/>
+        <source>Title:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="275"/>
+        <source>0:00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="currentsongwidget.ui" line="295"/>
+        <source>99:99</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="20"/>
@@ -39,82 +87,87 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="70"/>
+        <location filename="mainwindow.cpp" line="71"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="72"/>
-        <source>Artists</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.cpp" line="73"/>
-        <source>Albums</source>
+        <source>Show Song info</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="74"/>
-        <source>Playlist</source>
+        <source>Artists</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="75"/>
-        <source>Files</source>
+        <source>Albums</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="76"/>
+        <source>Playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="77"/>
+        <source>Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="78"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="133"/>
+        <location filename="mainwindow.cpp" line="139"/>
         <source>Default Server</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="133"/>
+        <location filename="mainwindow.cpp" line="139"/>
         <source>Please define a default server under server properties which is used for connection.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="273"/>
+        <location filename="mainwindow.cpp" line="285"/>
         <source>qmobilempd</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="273"/>
+        <location filename="mainwindow.cpp" line="285"/>
         <source>qMobileMPD Version:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="273"/>
+        <location filename="mainwindow.cpp" line="285"/>
         <source> Copyright 2011 by Hendrik Borghorst, licensed under GPLv3 for more information visit: &lt;a href=&apos;http://code.google.com/p/qmobilempd&apos;&gt;Homepage&lt;/a&gt; . This applications uses &lt;a href=&apos;http://www.tango-project.org&apos;&gt;Tango icons&lt;/a&gt;.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="419"/>
+        <location filename="mainwindow.cpp" line="421"/>
         <source>Connection error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="419"/>
+        <location filename="mainwindow.cpp" line="421"/>
         <source>Could not connect to default host check settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="408"/>
+        <location filename="mainwindow.cpp" line="410"/>
         <source>Not authenticated</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="68"/>
+        <location filename="mainwindow.cpp" line="69"/>
         <source>Connect to host</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="408"/>
+        <location filename="mainwindow.cpp" line="410"/>
         <source>Functions could be limited without password, check settings and reconnect</source>
         <translation type="unfinished"></translation>
     </message>
@@ -215,68 +268,68 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="167"/>
+        <location filename="ui_contextview.cpp" line="168"/>
         <source>Artists:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="214"/>
+        <location filename="ui_contextview.cpp" line="215"/>
         <source>Albums:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="341"/>
+        <location filename="ui_contextview.cpp" line="342"/>
         <source>Current playlist:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="658"/>
-        <location filename="ui_contextview.cpp" line="721"/>
+        <location filename="ui_contextview.cpp" line="664"/>
+        <location filename="ui_contextview.cpp" line="749"/>
         <source>Do you really want to add ALL albums to current playlist?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="866"/>
+        <location filename="ui_contextview.cpp" line="976"/>
         <source>Name</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="866"/>
+        <location filename="ui_contextview.cpp" line="976"/>
         <source>Please enter name for playlist.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="874"/>
+        <location filename="ui_contextview.cpp" line="984"/>
         <source>Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="874"/>
+        <location filename="ui_contextview.cpp" line="984"/>
         <source>Sorry save list could not get saved</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="900"/>
+        <location filename="ui_contextview.cpp" line="1010"/>
         <source>Saved playlists:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="907"/>
+        <location filename="ui_contextview.cpp" line="1017"/>
         <source>Clear playlist</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="907"/>
+        <location filename="ui_contextview.cpp" line="1017"/>
         <source>Are you sure to delete current playlist?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="919"/>
+        <location filename="ui_contextview.cpp" line="1029"/>
         <source>Quit?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui_contextview.cpp" line="919"/>
+        <location filename="ui_contextview.cpp" line="1029"/>
         <source>Are you sure to quit?</source>
         <translation type="unfinished"></translation>
     </message>
